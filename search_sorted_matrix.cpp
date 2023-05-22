bad2ac19f40e37df68288aa7624dd5483918ace8
Satyam <67496078+Kyouma45@users.noreply.github.com>
2023-05-22 10:36:28 +0530

    Create search_sorted_matrix.cpp

---
+++ search_sorted_matrix.cpp
@@ -0,0 +1,13 @@
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r=0,c=matrix[0].size()-1;
+        while(r<matrix.size()&&c>=0)
+        {
+            if(matrix[r][c]==target)
+                return 1;
+            else if(matrix[r][c]>target)
+                c--;
+            else
+                r++;
+        }
+        return 0;
+    }
