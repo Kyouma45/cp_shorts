dba7e2aa187cf9dc3d4b1b0df66eb6058dfcd039
Satyam <67496078+Kyouma45@users.noreply.github.com>
2023-08-13 22:20:25 +0530

    Create catalan.cpp

---
+++ catalan.cpp
@@ -0,0 +1,12 @@
+int mod=1e9+7;
+int catalan(int n)
+{
+  int dp[n+1];
+  dp[0]=1;
+  for(int i=1;i<=n;i++){
+    dp[i]=0;
+    for(int j=0;j<i;j++)
+      dp[i]=(dp[i]+(dp[j]*dp[i-j-1]%mod)%mod;
+  }
+  return dp[n];
+}
