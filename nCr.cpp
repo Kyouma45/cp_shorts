6d4a081dada995845d26de3aead7e60ab878cb14
Satyam <67496078+Kyouma45@users.noreply.github.com>
2024-02-18 16:00:12 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -7,3 +7,14 @@ int nCr(int n,int r)
       dp[j]=dp[j]+dp[j-1];
   return dp[r];
 }
+
+//if needed repeatedly
+//use pascal's triangle
+vector<vector<long long>> table;
+table.resize(n + 1);
+for (int i = 0; i < n + 1; ++i)
+    table[i] = vector<long long>(i + 1, 1);
+    for (int j = 1; j < i; ++j)
+        table[i][j] = (table[i - 1][j - 1] + table[i - 1][j]) % mod;
+
+// nCr=table[n][r]
